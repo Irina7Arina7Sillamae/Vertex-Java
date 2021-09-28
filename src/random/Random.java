2b383d4581cd48afefba2dc2f4d2cd8149ead13c
irina Siskova <Irina.Siskova@ivkhk.ee>
2021-09-28 16:24:04 +0300

    знакомство с random

---
+++ src/random/Random.java
@@ -0,0 +1,76 @@
+
+package random;
+
+public class Random {
+
+   
+    public static void main(String[] args) {
+        /*
+        //Генерация случайных чисел
+        int a = (int)(Math.random()*2+1);
+        System.out.println(a);*/
+
+        /*
+        //Округление чисел
+        float num = 6.75f;
+        System.out.println(Math.round(num));
+        System.out.println(Math.floor(num));
+        System.out.println(Math.ceil(num));*/
+        
+        /*
+        // min и max в java
+        float num1 = 4.025f;
+        int num2 = 5;
+         System.out.println("Большее число из двух чисел: " + Math.max(num1, num2));
+         System.out.println("Меньшее число из двух чисел: " + Math.min(num1, num2));*/
+
+        /*
+        //метод lenght() в Java - возвращает длину строки
+        String fraza = "Обожаю Java";
+        System.out.println(fraza.length());*/
+                
+        /*
+        //Метод concat() в Java - добавляет одну строку в конец другой строке
+         String fr = "У меня есть цель на ближайший год. ";
+         fr = fr.concat("Я должна стать Java разработчиком.");
+         System.out.println(fr);*/
+        
+        /*
+        //Метод charAt - возвращает символ стоящий на определенном индексе
+        String fr = "Осенняя пора очей очарованье";
+        char ch1 = fr.charAt(0);
+        char ch2 = fr.charAt(15);
+        char ch3 = fr.charAt(20);
+        System.out.println("Индекс 0 - это буква: " + ch1);
+        System.out.println("Индекс 15 - это буква: " + ch2);
+        System.out.println("Индекс 20 - это буква: " + ch3);*/
+        
+        /*
+        //Метод substring в java - возвращает часть строки по индексам
+        String fr = "Я спросила у кукушки,\n" +
+"Сколько лет я проживу…\n" +
+"Сосен дрогнули верхушки.\n" +
+"Желтый луч упал в траву.\n" +
+"Но ни звука в чаще свежей…\n" +
+"Я иду домой,\n" +
+"И прохладный ветер нежит\n" +
+"Лоб горячий мой.";
+        //System.out.println(fr);
+        //System.out.println(fr.substring(10));
+        System.out.println(fr.substring(11,20));*/
+                
+       
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+    }
+    
+}
